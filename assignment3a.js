5d22b3b8d1b969816e6fa5981e0fd4212ab33ae5
miawanjiru-cmyk <miawanjiru7@gmail.com>
2026-02-27 16:50:14 +0300

    comit

---
+++ assignment3a.js
@@ -0,0 +1,8 @@
+function mycountry(){
+    console.log("my country is kenya")
+    console.log("it has 47 counties")
+    console.log("my country has beautiful sandy beaches")
+    console.log("my country has a wonder full culture")
+    console.log("my country is a corrupt country")
+}
+mycountry()
